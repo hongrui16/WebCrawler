d1fd2cf3e50ab778d23daa8fb30dd6c1b69fb6cb
hongrui16 <smarthong@qq.com>
2017-09-04 15:10:27 +0800

    第一次正确运行版本

--- VideoCrawler/pachong.py
+++ VideoCrawler/pachong.py
@@ -1,4 +1,7 @@
 # -*- coding:utf-8 -*-
+#--------------视频爬虫---------------
+#--------------洪瑞------------------
+#--------------2017年9月4日----------
 from Tkinter import *
 from ScrolledText import ScrolledText
 import urllib
@@ -17,20 +20,19 @@ def get():
     url='http://www.budejie.com/video/'+str(a)
     varl.set('已经获取第%s页的视频'%(a))
     html=requests.get(url,headers=hd).text#发送请求
- #   print html
     url_content = re.compile(r'(<div class="j-r-list-c">.*?</div>.*?</div>)',re.S)
     url_contents = re.findall(url_content,html)
     for i in url_contents:
-        url_reg = r'data-mp4="(.*?)">'
+        url_reg = r'data-mp4="(.*?)"'
         url_items = re.findall(url_reg,i)
-        print url_items
         if url_items:#如果有视频存在，就匹配名字，如果是图片，就跳过
-            name_reg = re.compile(r'<a href="detail-.{8}.html">(.*?)</a>', re.S)
+            name_reg = re.compile(r'<a href="/detail-.{8}.html">(.*?)</a>', re.S)
             name_items = re.findall(name_reg,i)
            # print name_items
-            for i,k in zip(name_items,url_items):
+            for i,k in zip(name_items,url_items):#zip这个内置函数就是将List重新整合一下
                 url_name.append([i,k])
- #               print i,k
+                print i,k
+    print url_name
     return url_name
 
 id =1 #视频个数
@@ -39,8 +41,9 @@ def write():
     while id<10:
         url_name = get()#调用获取视频+名字
         for i in url_name:
-            urllib.urlretrieve(i[1],'video/%s.mp4'%(i[0].decode('utf-8').encode('gbk')))#下载
-            text.insert(END,str(id)+'.'+i[1]+'\n'+i[0]+'\n')
+            text.insert(END, str(id) + '.' + i[1] + '\n' + i[0] + '\n')
+         #   urllib.urlretrieve(i[1],'~/videoCrawler/%s.mp4'%(i[0].decode('utf-8').encode('gbk')))#下载
+            urllib.urlretrieve(i[1], '/home/dahua/videoCrawler/%s.mp4' % (i[0]))  # 下载
             url_name.pop(0)#删除第一个元素
             id+=1
     varl.set('视频抓取完毕，over')
@@ -50,15 +53,22 @@ def start():
     th.start()
 
 
+# 创建一个窗口
 root = Tk()
+# 窗口的标题
 root.title('bangbangda')
+# 文本滚动窗口
 text = ScrolledText(root,font=('Courier New',10))
 text.grid()
+
 button = Button(root,text = 'start to crawl',font =('微软雅黑',10),command=start)
 button.grid()
+
+# 设置Lable
 varl = StringVar()#通过一个tk方法绑定一个变量
 label = Label(root,font=('微软雅黑',10),fg='red',textvariable=varl)
 label.grid()
+
 varl.set('Ready!')
 root.mainloop()
 
